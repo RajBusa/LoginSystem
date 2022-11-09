c0a6d8818a53e0163a23b295bb6396adb6cba214
RajBusa <20it0112charusat.edu.in>
2022-11-09 10:59:25 +0530

    first commit

---
+++ Backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/userLoginSys";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connect to mongo successfully");
+    })
+}
+
+module.exports = connectToMongo; 
